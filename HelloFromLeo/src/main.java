2dab7a078f8d996a39dbc6d56c14fb538ab7fdf6
pnxokratos <her19054@byui.edu>
2021-05-22 01:45:01 +0200

    TESTING FOR PROVE04 ASSIGNMENT

--- HelloFromLeo/src/main.java
+++ HelloFromLeo/src/main.java
@@ -3,6 +3,7 @@ public class main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello, my name is Leo.\n");
+        System.out.println("Hello, I'm testing commitments.\n");
     }
 }
 
